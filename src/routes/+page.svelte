d280150e629473bdf56eae8be3f53b183da35e7b
Jacob <jacobcarl88@gmail.com>
2024-09-14 19:15:28 +0200

    add basic homepage layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script>
-  import LeafletMap from "$lib/LeafletMap.svelte";
   import Nav from "$lib/Nav.svelte";
   import Toast from "$lib/Toast.svelte";
+  import WeatherCard from "$lib/WeatherCard.svelte";
 </script>
 
-<main class="relative">
-  <Toast type="info" />
-  <LeafletMap />
+<main class="relative min-h-screen pb-20">
+  <Toast type="success" duration={5000}>Toast is working nicely</Toast>
+
+  <!-- current card info -->
+
+  <section class="px-4 pt-4 flex flex-col gap-4">
+    <p>--Location--</p>
+    <WeatherCard />
+    <WeatherCard />
+    <WeatherCard />
+    <WeatherCard />
+    <WeatherCard />
+  </section>
+
   <Nav />
 </main>
