78823a2dd39a51793de4ec605c4e1bbf64392449
Jacob <jacobcarl88@gmail.com>
2024-09-14 19:14:07 +0200

    add routes

---
+++ src/routes/info/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+Info Page Data Credits
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import LeafletMap from "$lib/LeafletMap.svelte";
+  import MapMenu from "$lib/MapMenu.svelte";
+  import Nav from "$lib/Nav.svelte";
+  import Toast from "$lib/Toast.svelte";
+</script>
+
+<main class="relative">
+  <Toast type="success" duration={5000}>Toast is working nicely</Toast>
+  <LeafletMap />
+  <MapMenu />
+  <Nav />
+</main>
