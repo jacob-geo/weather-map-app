67b7e528747bd729432a31658236aff752db2ae0
Jacob <jacobcarl88@gmail.com>
2024-09-14 19:14:43 +0200

    implement gsap animated toast component

--- src/lib/Toast.svelte
+++ src/lib/Toast.svelte
@@ -1,16 +1,61 @@
-<!-- a toast for displaying messages (Errors, infos and warnings) -->
+<!-- toast component with gsap animation to show messages -->
 
 <script lang="ts">
-  export let type: "success" | "info" | "warning" | "error";
+  import { gsap } from "gsap";
+  import { onMount } from "svelte";
+
+  export let type: "success" | "info" | "warning" | "error" = "info";
+
+  export let duration = 3000;
+
+  const ToastTypeIconMap = {
+    success: "👍",
+    info: "😞",
+    warning: "⚠️",
+    error: "❌",
+  };
+
+  onMount(() => {
+    let tl = gsap.timeline();
+    tl.from(".toast", {
+      duration: 0.3,
+      width: "3rem",
+      height: "3rem",
+    })
+      .to(".toast", {
+        duration: 0.3,
+        top: "1rem",
+        ease: "power2.inOut",
+      })
+      .to(".toast", {
+        duration: 0.5,
+        delay: 0.3,
+        width: "60vw",
+        ease: "power2.inOut",
+      })
+      .to(".toast-text", {
+        display: "block",
+        delay: 0.2,
+      });
+
+    setTimeout(() => {
+      tl.reverse();
+    }, duration);
+  });
 </script>
 
-<!-- TODO icons depending on type -->
-
 <div
-  class="absolute top-4 left-1/2 -translate-x-1/2 bg-black h-12 w-[50vw] z-[9999] rounded-full flex items-center gap-4 p-4"
+  class="toast absolute -top-full left-1/2 -translate-x-1/2 bg-black z-[9999] rounded-full flex items-center gap-2 p-2 text-white"
 >
   <!-- toast icon -->
-  <div>🧠</div>
+  <div
+    class=" w-10 h-10 border border-gray-300 rounded-full flex items-center justify-center"
+  >
+    {ToastTypeIconMap[type]}
+  </div>
+
   <!-- toast text content -->
-  <p class="text-white">Toast content {type}</p>
+  <p class="toast-text hidden">
+    <slot />
+  </p>
 </div>
