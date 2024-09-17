ad125580a0fbe8ae1a463aff0ce0132687887ea0
Jacob <jacobcarl88@gmail.com>
2024-09-17 14:04:56 +0200

    change /info to /settings + notes

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+Settings - see current location details - ability to change default location -
+ability to add new locations to list (max. 5) - ability to add more wms
+connections (with naming, url, and notes options) + change order of layers -
+layer and app credits
